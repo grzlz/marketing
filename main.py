7cb42460dc3a08d30d174b42075e9a0916a55779
AndresP <andukial@gmail.com>
2023-07-19 08:09:11 -0600

    Commit 2

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-import os
-import boto3
-from dotenv import load_dotenv
-from PIL import Image, ImageDraw, ImageFont
+# import os
+# import boto3
+# from dotenv import load_dotenv
+# from PIL import Image, ImageDraw, ImageFont
 
-load_dotenv()  # take environment variables from .env.
+# load_dotenv()  # take environment variables from .env.
 
-# Now you can access the variables. They are stored as strings.
-# db_user = os.getenv('example')
+# # Now you can access the variables. They are stored as strings.
+# # db_user = os.getenv('example')
 
-client = boto3.client('rekognition')
-
-
-photo = input("What image do you want to upload? Options: fachada.jpg, lobby.jpg ")
+# client = boto3.client('rekognition')
 
 
-with open(photo, "rb") as image:
-    source_bytes = image.read()
+# photo = input("What image do you want to upload? Options: fachada.jpg, lobby.jpg ")
 
-detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
-labels = [i["Name"] for i in detect_objects["Labels"]]
 
-for label in labels:
-    print(f"The next label is: {label}")
+# with open(photo, "rb") as image:
+#     source_bytes = image.read()
+
+# detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
+# labels = [i["Name"] for i in detect_objects["Labels"]]
+
+# for label in labels:
+#     print(f"The next label is: {label}")
+labels = ['Architecture', 'Building', 'Office Building', 'City', 'Condo', 'Housing', 'Urban', 'High Rise', 'Apartment Building', 'Tower', 'Car', 'Transportation', 'Vehicle', 'Convention Center']
 
 def refine_data(list_items):
     print(list_items)
